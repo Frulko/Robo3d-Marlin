31acbe738118c701c19072f95ea13c90908e8827
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2022-12-10 17:44:30 +0100

    feat: add biqu h2 steps per unit value

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1169,7 +1169,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2267.72, 723.38 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2267.72, 932 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
